cbdb2a28ffa340ef7da29b7d2e9a897e02fdb39f
basyura <basyura@gmail.com>
2015-03-04 20:55:04 +0900

    improved readme

--- init.rb
+++ init.rb
@@ -5,13 +5,13 @@ Redmine::Plugin.register :redmine_diff_mail do
   author 'basyura name'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
+  url 'https://github.com/basyura/redmine_diff_mail'
   author_url 'http://basyura.org'
 end
 
 ActionDispatch::Callbacks.to_prepare do
   require_dependency 'wiki_page'
-	WikiPage.send(:include, DiffMail::WikiPagePatch)
+  WikiPage.send(:include, DiffMail::WikiPagePatch)
 end
 
 require 'wiki_page_patch.rb'
